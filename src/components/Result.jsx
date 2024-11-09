0922e8a5ca3825c278fcc385b0828cd57293e498
Verónica I Pérez S <v.isabel1989@gmail.com>
2024-11-09 15:55:57 +0100

    css OK. modificacion de Result.jsx; se retorna con el metodo map para abarcar cada parteeeeeeeeeeee y poder manejar cada parte con css(burbujas)

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,6 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useCalculator } from './CalculatorProvider';
+import { useCalculator } from './CalculatorProvider'
+
+const Result = () => {
+  const { numbers, result, resetCalculator } = useCalculator();
+
+  return (
+    <>
+    <div className="result">
+      <h2>Resultado</h2>
+      <h3>Suma de números:</h3>
+      <div className="selected-numbers">
+        {numbers.map((num, index) => (
+          <span key={index} className="number-bubble">{num}</span>
+        ))}
+      </div>
+      <div className="total-result">Total: {result}</div>
+      <button onClick={resetCalculator} className='reset'>Resetear</button>
+      <Link to="/">Volver a la calculadora</Link>
+    </div>
+    </>
+  );
+};
+
+export default Result;
+
+
+/*
+utiliza join en "selected-nubers"
 
 const Result = () => {
   const { numbers, result, resetCalculator } = useCalculator();
@@ -17,3 +44,5 @@ const Result = () => {
 };
 
 export default Result;
+
+*/
